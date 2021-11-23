5ded849c192c3bd7f5a7aa8387d6487572dce35c
Caleb Rodriguez <sirdigo6@gmail.com>
2021-11-22 22:05:16 -0500

    commit tree 087b61633a82eb425270b5730a6812bf57e9d087

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const num3 = 14;
+const num4 = 10;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() *10) +1;
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 5, 3, 9, 13, 20);
